b05ef3f3b06564f580143375b3b4afb04cd1bac4
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-10-31 09:10:15 -0600

    Create fiveCharactersOrFewerOnly.js

---
+++ fiveCharactersOrFewerOnly.js
@@ -0,0 +1,6 @@
+fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"])
+
+function peopleWhoBelongToTheIlluminati(arr){
+  const output = arr.filter(char => char.member === true)
+  return output;
+}
