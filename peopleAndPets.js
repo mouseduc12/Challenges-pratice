9847995ec3685c8c2aa02bb52c0c861dcf6b3e11
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-11-17 13:16:30 -0700

    Create peopleAndPets.js

---
+++ peopleAndPets.js
@@ -0,0 +1,95 @@
+const peopleAndPets = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47,
+        pets: [
+            {
+                name: "Alfred",
+                type: "dog",
+                nickNames: ["Lil Alfred", "Alfy", "Alfinator"]
+            },
+            {
+                name: "Charles",
+                type: "cat",
+                nickNames: ["Charley"]
+            },
+            {
+                name: "Bark Obama",
+                type: "dog",
+                nickNames: ["Barack", "Mr. President"]
+            },
+            {
+                name: "Christopher George Latore Wallace",
+                type: "dog",
+                nickNames: ["Notorious D.I.G.", "Diggie Smalls"]
+            }
+
+        ]
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12,
+        pets: [
+            {
+                name: "Howard",
+                type: "dog",
+                nickNames: []
+            },
+            {
+                name: "Bear",
+                type: "dog",
+                nickNames: []
+            }
+        ]
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78,
+        pets: [
+            {
+                name: "Bird Person",
+                type: "bird",
+                nickNames: ["Phoenixperson"]
+            },
+            {
+                name: "Krombopulos Michael",
+                type: "Gromflomite",
+                nickNames: ["Assassin man"]
+            },
+            {
+                name: "Squanchy",
+                type: "Cat-person",
+                nickNames: ["Squanch", "Smarf", "Thunder Cat"]
+            }
+
+        ]
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 23,
+        pets: [
+            {
+                name: "Morty Jr.",
+                type: "Gazorpazorp",
+                nickNames: ["Gwendolyn Jr."]
+            },
+            {
+                name: "Snuffels",
+                type: "dog",
+                nickNames: ["Snowball"]
+            }
+
+        ]
+    }
+];
+
+const findPets = (array) => {
+  const newArr = []
+  const output = array.map(each => (each.pets.map(pet => newArr.push(pet.name))));
+  return newArr;
+}
+console.log(findPets(peopleAndPets))
