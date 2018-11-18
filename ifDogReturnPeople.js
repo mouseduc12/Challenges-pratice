3cb5d77d4f3a544ef416879af23d1bb029b91058
Pham <33107074+mouseduc12@users.noreply.github.com>
2018-11-18 09:38:08 -0700

    Create ifDogReturnPeople.js

---
+++ ifDogReturnPeople.js
@@ -0,0 +1,105 @@
+const peopleAndPets = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47,
+        pets: [
+            {
+                name: "Alfred",
+                type: "dog",
+                nickNames: ["Lil Alfred", "Alfy", "Alfinator"]
+            },
+            {
+                name: "Charles",
+                type: "cat",
+                nickNames: ["Charley"]
+            },
+            {
+                name: "Bark Obama",
+                type: "dog",
+                nickNames: ["Barack", "Mr. President"]
+            },
+            {
+                name: "Christopher George Latore Wallace",
+                type: "dog",
+                nickNames: ["Notorious D.I.G.", "Diggie Smalls"]
+            }
+
+        ]
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12,
+        pets: [
+            {
+                name: "Howard",
+                type: "dog",
+                nickNames: []
+            },
+            {
+                name: "Bear",
+                type: "dog",
+                nickNames: []
+            }
+        ]
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78,
+        pets: [
+            {
+                name: "Bird Person",
+                type: "bird",
+                nickNames: ["Phoenixperson"]
+            },
+            {
+                name: "Krombopulos Michael",
+                type: "Gromflomite",
+                nickNames: ["Assassin man"]
+            },
+            {
+                name: "Squanchy",
+                type: "Cat-person",
+                nickNames: ["Squanch", "Smarf", "Thunder Cat"]
+            }
+
+        ]
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 23,
+        pets: [
+            {
+                name: "Morty Jr.",
+                type: "Gazorpazorp",
+                nickNames: ["Gwendolyn Jr."]
+            },
+            {
+                name: "Snuffels",
+                type: "dog",
+                nickNames: ["Snowball"]
+            }
+
+        ]
+    }
+];
+
+//Create an array of everyone that has at least one dog and sort them by age.
+const ifDog = arr =>{
+  let newArr = []
+  for(let i = 0 ; i < arr.length; i++){
+    for(let j = 0; j < arr[i].pets.length; j++){
+        if(arr[i].pets[j].type === "dog"){
+          newArr.push(arr[i]);
+      }
+    }
+  }
+  let output = newArr.filter((each, index, arr) => index === arr.indexOf(each));
+  console.log(output);
+  output.sort((a,b) => a.age - b.age);
+  console.log(output);
+}
+ifDog(peopleAndPets);
