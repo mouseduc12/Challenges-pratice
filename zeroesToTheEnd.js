8b435f7bbb358d1a5ff31380343fded412d887b9
Pham <33107074+mouseduc12@users.noreply.github.com>
2019-02-16 21:27:58 -0700

    Rename moveZeroes.js to zeroesToTheEnd.js

---
+++ zeroesToTheEnd.js
@@ -0,0 +1,14 @@
+// var moveZeroes = function(nums) {
+//     const newArr = [];
+//     for(let i = 0; i < nums.length; i++){
+//       if(nums[i] === 0){
+//         newArr.push(parseInt(nums.splice(i, 1).toString()))
+//         i--
+//       }
+//     }
+//     // console.log(newArr);  
+//      nums.push(...newArr)
+//      return nums
+// };
+
+// moveZeroes([1,2,2,3,3,3,3,0,1,2,3,4,0,2,1,0,2,1,0])
