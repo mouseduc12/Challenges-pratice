6f35211120c1c2993848c08ab2d6431537c7ee09
Pham <33107074+mouseduc12@users.noreply.github.com>
2019-02-17 19:51:33 -0600

    Create rotatePosition.js

---
+++ rotatePosition.js
@@ -0,0 +1,12 @@
+// var rotate = function (nums, k) {
+//   for (let i = 0; i < k; i++) {
+//     let current = nums.pop()
+//     console.log(current)
+//     nums.unshift(current)
+//   }
+//   return nums
+// };
+
+// // rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// // rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// // rotate 3 steps to the right: [5,6,7,1,2,3,4]
