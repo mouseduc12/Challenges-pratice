eb58146a43333cae76be8d159dae121bcf24972c
Pham <33107074+mouseduc12@users.noreply.github.com>
2019-03-06 22:12:49 +0530

    Create bigOExercise.js

---
+++ bigOExercise.js
@@ -0,0 +1,11 @@
+function funChallenge(input) {
+  let a = 10; //1
+  a = 50 + 3; //1
+
+  //O(n)
+  for (let i = 0; i < input.length; i++) {
+    anotherFunction(); //O(n)
+    let stranger = true; //O(n)
+    a++; // O(n)
+  }
+  return a;
