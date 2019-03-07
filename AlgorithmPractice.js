e4c536101c6a80599941728c2b42523747fe20f2
Pham <33107074+mouseduc12@users.noreply.github.com>
2019-03-07 20:12:39 +0530

    Create AlgorithmPractice.js

---
+++ AlgorithmPractice.js
@@ -0,0 +1,28 @@
+class Node{
+  constructor(data, left = null, right = null){
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinarySearchTree{
+  constructor(){
+    this.roof = nul;
+  }
+  add(data){
+    const node = this.roof;
+    if(node === null){
+      this.roof = new Node(data);
+      return;
+    } else {
+      const searchTree = function(node){
+        if(data < node.data){
+          if(node.left === null){
+            node.left = new Node(data)
+          }
+        }
+      }
+    }
+  }
+}
