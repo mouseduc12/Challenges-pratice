f75c1011960c65ca50d762dcbda23b8700300846
Pham <33107074+mouseduc12@users.noreply.github.com>
2019-03-15 12:35:04 +0530

    Create InvestorLastAndFirst.js

---
+++ InvestorLastAndFirst.js
@@ -0,0 +1,14 @@
+// const people =["John", "Dick", "Anal", "Necro", "Bomb", "Pete", "Pinkman", "Walter", "Anna", "WalterJr", "Flynn","Hank", "Alle", "Lily", "Washington", "Olivia", "Tyler", "Eric", "Nate", "Marcus", "Tim", "Scott", "Andrew"]
+
+// //Filter the list of investors for those whose were born in the 1500'
+// const output1 = investors.filter(each => each.year >= 1501 && each.year <= 1601)
+
+// //give us an array of the investors first and last name
+
+// const output2 = investors.map(each => {
+//   return {
+//     first: each.first,
+//     last: each.last
+//   }
+// })
+// // console.log(output2)
