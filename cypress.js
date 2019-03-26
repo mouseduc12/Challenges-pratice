70e2a13c1ef6ffb92fc4275eff7a710625950c7e
Pham <33107074+mouseduc12@users.noreply.github.com>
2019-03-26 10:25:16 -0400

    Create cypress.js

---
+++ cypress.js
@@ -0,0 +1,31 @@
+// import EmailForm from "./EmailForm.vue"
+// import { shallowMount, mount } from "@vue/test-utils"
+
+// describe("Email Form", () =>{
+//   it("Should be Component", () => {
+//     const wrapper = shallowMount(EmailForm)
+    
+//     const input = wrapper.find('input[name=email]')
+//     input.setValue = "someThing@gmail.com"
+//     input.trigger('input')
+
+//     expect(wrapper.vm.email).toBe("someThing@gmail.com")
+//     const spy = jest.spyOn(wrapper.vm, 'submit')
+//     wrapper.find('[data-test="submit]').trigger('click')
+//   })
+// })
+
+// describe("Grid Item", () => {
+//   it("It should be grid", () =>{
+//     const wrapper = shallowMount(GridItem, {
+//       propsData: {
+//         title: 'Lemondrop',
+//         image: 'https://picsum.photo/1',
+//         subtitle: 'The Flat'
+//       }
+//     })
+
+//     expect()
+
+//   })
+// })
